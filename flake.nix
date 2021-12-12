8f2b2afa4a7b7dbf5f6abb0b6b9e71ae9a1006cd
ilkecan <ilkecan@protonmail.com>
2021-12-13 00:21:36 +0300

    Use nixos-21.11

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.11";
     nix-filter.url = "github:ilkecan/nix-filter";
     android-nixpkgs = {
       url = "github:tadfisher/android-nixpkgs/stable";
