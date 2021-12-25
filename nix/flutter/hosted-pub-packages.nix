c5fc08eda534e9a2ca4a4fd6048d2c1ba355b92b
ilkecan <ilkecan@protonmail.com>
2021-12-25 20:28:38 +0300

    Don't unnecessarily IFD

--- nix/flutter/hosted-pub-packages.nix
+++ nix/flutter/hosted-pub-packages.nix
@@ -1,34 +1,15 @@
 {
   callPackage,
   hostedPubPackages,
-  lib,
-  writeText,
 }:
 
 let
-  inherit (lib)
-    concatMapStrings
-  ;
-
-  inherit (lib.strings)
-    escapeNixString
-  ;
-
   fetchPub = callPackage ./fetch-pub.nix { };
-  fetch = {name, version, hash, url}: ''
-    (fetchPub {
-      name = ${escapeNixString name};
-      version = ${escapeNixString version};
-      sha256 = ${escapeNixString hash};
-      url = ${escapeNixString url};
-    })
-  '';
+  fetch = {name, version, hash, url}: fetchPub {
+    inherit name version url;
+    sha256 = hash;
+  };
 
-  hostedPubPackagesNix = writeText "hosted-pub-packages.nix" ''
-    fetchPub:
-    [
-    ${concatMapStrings fetch hostedPubPackages}
-    ]
-  '';
+  hostedPubPackageDrvs = map fetch hostedPubPackages;
 in
-import hostedPubPackagesNix fetchPub
+hostedPubPackageDrvs
